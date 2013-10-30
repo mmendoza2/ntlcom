8ccdccb24492eeb6d4ecfd04ba9dabb817495c4e
mmendoza2 <man.m25@gmail.com>
2013-10-30 15:13:21 -0600

    Mejorando Loguin Facebook

--- app/controllers/notelimites_controller.rb
+++ app/controllers/notelimites_controller.rb
@@ -1,6 +1,8 @@
 class NotelimitesController < ApplicationController
 
+
   def home
+
     if signed_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
@@ -16,5 +18,9 @@ class NotelimitesController < ApplicationController
   end
 
   def contacto
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,13 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '182819485154177', 'd0a0ccde76db8a64187351ffa7f8d465',
+  if Rails.env.development?
+  provider :facebook, '626384007393278', 'c8e03770698ac3fa74828538c16dceb9',
            scope: "email, publish_stream",
            :secure_image_url => 'true'
+  else
+ provider :facebook, '182819485154177', 'd0a0ccde76db8a64187351ffa7f8d465',
+          scope: "email, publish_stream",
+          :secure_image_url => 'true'
+  end
 end
