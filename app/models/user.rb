8c51cf24783d5a76add08fcb737631258efb8d34
mmendoza2 <man.m25@gmail.com>
2013-11-01 15:17:50 -0600

    Email Facebook Omniauht arreglado

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,7 +90,7 @@ class User < ActiveRecord::Base
         user.provider = auth.provider
         user.uid = auth.uid
         user.name = auth.info.name
-        user.email  = auth.email
+        user.email  = auth.info.email
         user.oauth_token = auth.credentials.token
         user.oauth_expires_at = Time.at(auth.credentials.expires_at)
         user.avatar = auth.info.image
