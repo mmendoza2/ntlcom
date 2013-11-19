01994b651df122b60611dc2bd103787845b347ec
mmendoza2 <man.m25@gmail.com>
2013-11-19 17:01:22 -0600

    Problema de config devise arreglado

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,7 +234,7 @@ Devise.setup do |config|
                   scope: "email, publish_stream",
                   :secure_image_url => 'true'
   else
-    provider :facebook, '182819485154177', 'd0a0ccde76db8a64187351ffa7f8d465',
+    config.omniauth :facebook, '182819485154177', 'd0a0ccde76db8a64187351ffa7f8d465',
                 scope: "email, publish_stream",
                 :secure_image_url => 'true'
   end
